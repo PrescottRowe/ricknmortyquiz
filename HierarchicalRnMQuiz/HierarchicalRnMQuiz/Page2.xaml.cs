d4a34e19a7e789dca7b5b3599dbda342232ada24
Prescott Rowe <prescottrowe@gmail.com>
2020-02-18 16:08:37 -0800

    Add project files.

---
+++ HierarchicalRnMQuiz/HierarchicalRnMQuiz/Page2.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HierarchicalRnMQuiz
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+        async void GoToNextPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page3());
+        }
+        async void GoToPreviousPage(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+        async void GoToMainPage(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+        void OnCheckBoxCheckedChanged1(object sender, CheckedChangedEventArgs e)
+        {
+            Console.WriteLine("1");
+        }
+        void OnCheckBoxCheckedChanged2(object sender, CheckedChangedEventArgs e)
+        {
+            Console.WriteLine("2");
+        }
+        void OnCheckBoxCheckedChanged3(object sender, CheckedChangedEventArgs e)
+        {
+            Console.WriteLine("3");
+        }
+        void OnCheckBoxCheckedChanged4(object sender, CheckedChangedEventArgs e)
+        {
+            Console.WriteLine("4");
+        }
+        void OnCheckBoxCheckedChanged5(object sender, CheckedChangedEventArgs e)
+        {
+            Console.WriteLine("5");
+        }
+    }
+}
---
+++ HierarchicalRnMQuiz/HierarchicalRnMQuiz/Page6.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HierarchicalRnMQuiz
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page6 : ContentPage
+    {
+        public Page6()
+        {
+            InitializeComponent();         
+        }
+        async void GoToMainPage(object sender, EventArgs e)
+        {
+            await Navigation.PopToRootAsync();
+        }
+    }
+}
